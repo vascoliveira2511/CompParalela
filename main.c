4fe03b5df572242610ce87781e71496776e78924
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-10-19 16:13:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define N 10000000
+#define K 4
+
+void inicializar(float *px, float *py, float *cx, float *cy)
+{
+    srand(10);
+    for (int i = 0; i < N; i++)
+    {
+        px[i] = (float)rand() / RAND_MAX;
+        py[i] = (float)rand() / RAND_MAX;
+    }
+
+    for (int i = 0; i < K; i++)
+    {
+        cx[i] = px[i];
+        cy[i] = py[i];
+    }
+}
+
+int main()
+{
+    printf("N = %i, K = %i", N, K);
+}
