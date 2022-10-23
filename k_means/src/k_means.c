69c6c169c836c0b7958bd32ed45933d6d08e0e7c
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-10-23 14:48:52 +0100

    Update k_means.c

--- k_means/src/k_means.c
+++ k_means/src/k_means.c
@@ -5,15 +5,17 @@
 #define K 4
 #define N 10000000
 
-typedef struct PointStruct {
+typedef struct PointStruct
+{
 	float x, y;
 	int assigned_cluster;
-} *pPoint, Point;
+} * pPoint, Point;
 
-typedef struct ClusterStruct {
+typedef struct ClusterStruct
+{
 	Point centroid;
 	int size;
-} *pCluster, Cluster;
+} * pCluster, Cluster;
 
 void printPoint(Point p)
 {
@@ -29,15 +31,15 @@ void printCluster(Cluster c)
 
 float eucDist(Point a, Point b)
 {
-	return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+	return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
 void populateSamples(Point arr[])
 {
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		arr[i].x = ((float) rand() / (float) (RAND_MAX));
-		arr[i].y = ((float) rand() / (float) (RAND_MAX));
+		arr[i].x = ((float)rand() / (float)(RAND_MAX));
+		arr[i].y = ((float)rand() / (float)(RAND_MAX));
 		arr[i].assigned_cluster = -1;
 	}
 }
@@ -51,28 +53,29 @@ void initCluster(pCluster c, Point p)
 
 void initClusters(Cluster clusters[], Point samples[])
 {
-	for(int i = 0; i < K; i++)
+	for (int i = 0; i < K; i++)
 		initCluster(&clusters[i], samples[i]);
 }
 
 int assignSamples(Cluster clusters[], Point samples[])
 {
 	int changed = 0;
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		int nearest_idx = 0;
-		for(int j = 0; j < K; j++)
+		for (int j = 0; j < K; j++)
 		{
-			if(eucDist(samples[i],clusters[j].centroid) < eucDist(samples[i], clusters[nearest_idx].centroid))
+			if (eucDist(samples[i], clusters[j].centroid) < eucDist(samples[i], clusters[nearest_idx].centroid))
 				nearest_idx = j;
 		}
 		if (samples[i].assigned_cluster == nearest_idx)
 			continue;
-		else {
-			clusters[samples[i].assigned_cluster].size --;
+		else
+		{
+			clusters[samples[i].assigned_cluster].size--;
 			samples[i].assigned_cluster = nearest_idx;
-			clusters[nearest_idx].size ++;
-			changed ++;
+			clusters[nearest_idx].size++;
+			changed++;
 		}
 	}
 	return changed;
@@ -81,24 +84,22 @@ int assignSamples(Cluster clusters[], Point samples[])
 void updateCentroids(Cluster clusters[], Point samples[])
 {
 	Cluster new_clusters[K];
-	for(int i = 0; i < K; i++)
+	for (int i = 0; i < K; i++)
 	{
 		new_clusters[i].centroid.x = 0.f;
 		new_clusters[i].centroid.y = 0.f;
 	}
 
-
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		new_clusters[samples[i].assigned_cluster].centroid.x += samples[i].x;
 		new_clusters[samples[i].assigned_cluster].centroid.y += samples[i].y;
 	}
 
-
-	for(int i = 0; i < K; i++)
+	for (int i = 0; i < K; i++)
 	{
-		clusters[i].centroid.x = new_clusters[i].centroid.x / (float) clusters[i].size;
-		clusters[i].centroid.y = new_clusters[i].centroid.y / (float) clusters[i].size;
+		clusters[i].centroid.x = new_clusters[i].centroid.x / (float)clusters[i].size;
+		clusters[i].centroid.y = new_clusters[i].centroid.y / (float)clusters[i].size;
 	}
 }
 
@@ -108,8 +109,7 @@ int main()
 
 	Point *samples = malloc(N * sizeof(Point));
 	Cluster clusters[K];
-	
-	
+
 	populateSamples(samples);
 	initClusters(clusters, samples);
 	assignSamples(clusters, samples);
@@ -122,10 +122,10 @@ int main()
 		updateCentroids(clusters, samples);
 		changed = assignSamples(clusters, samples);
 
-	} while(changed>0);
+	} while (changed > 0);
 
 	printf("N = %d, K = %d\n", N, K);
-	for(int i = 0; i < K; i++)
+	for (int i = 0; i < K; i++)
 		printCluster(clusters[i]);
 	printf("Iterations: %d\n", iterations);
 
